65c188bb5a98543187c88cd1b1e7f99f603a88dd
Alexey Alekhin <laughedelic@gmail.com>
2013-08-27 19:07:20 +0200

    Fixed usage of velvet

--- build.sbt
+++ build.sbt
@@ -6,7 +6,12 @@ homepage := Some(url("https://github.com/ohnosequences/velvet-task"))
 
 licenses := Seq("AGPLv3" -> url("http://www.gnu.org/licenses/agpl-3.0.txt"))
 
-// fully qualified name(s) of your bundle object(s)
-bundleObjects := Seq("ohnosequences.statika.VelvetTask")
+bundleObjects := Seq(
+  "ohnosequences.statika.VelvetTask"
+, "ohnosequences.statika.SpecialVelvet"
+)
 
-libraryDependencies ++= Seq( "ohnosequences" %% "velvet" % "0.1.0" ,  "ohnosequences" %% "s3cmd" % "0.1.0" ) 
+libraryDependencies ++= Seq(
+  "ohnosequences" %% "velvet" % "0.1.1-SNAPSHOT"
+, "ohnosequences" %% "s3cmd" % "0.1.0"
+) 
--- src/main/scala/VelvetTask.scala
+++ src/main/scala/VelvetTask.scala
@@ -1,14 +1,43 @@
 package ohnosequences.statika
 
 import ohnosequences.statika._
+import sys.process._
 
-case object VelvetTask extends Bundle(Velvet :: S3cmd :: HNil) {
+case object SpecialVelvet extends VelvetOpts(
+	categories = 5
+  , maxKmerLength = 99
+  ) {
+  val metadata = generated.metadata.SpecialVelvet
+}
+
+case object VelvetTask extends Bundle(SpecialVelvet :: S3cmd :: HNil) {
 
   val metadata = generated.metadata.VelvetTask
 
+  // Adding method to run commands from a given path
+  implicit class PBAt(cmd: Seq[String]) {
+    implicit def @@(path: String): ProcessBuilder =
+      Process(cmd, new java.io.File(path), "" -> "")
+  }
+
   def install[D <: DistributionAux](distribution: D): InstallResults = {
-    // do someting here
-    success("%s is installed" format metadata)
+
+    val trace = List[ProcessBuilder](
+      Seq("mkdir", "task")
+    , Seq("s3cmd", "get", "s3://team3-resources/reads.tgz") @@ "task"
+    , Seq("tar", "-xvf", "./reads.tgz") @@ "task"
+    , Seq("velveth", "./assembly-folder", "99", "-short", "-fastq", "reads.fastq") @@ "task"
+    , Seq("velvetg", "./assembly-folder", "-exp_cov", "auto", "-read_trkg", "yes") @@ "task"
+    , Seq("s3cmd", "put", "--recursive", "assembly-folder/", 
+    		"s3://team3-results/alexey/assembly/") @@ "task"
+    ).foldLeft(List[InstallResult]()){ (acc, cmd) =>
+      if (acc exists (_.isLeft)) acc
+      else if (cmd.! != 0) acc ::: failure(cmd.toString)
+      else acc ::: success(cmd.toString)
+    }
+
+    if (trace exists (_.isLeft)) trace
+    else success("%s is installed" format metadata)
   }
 
 }
